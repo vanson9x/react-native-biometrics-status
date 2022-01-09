319feab43d1d60d319f50167029e1f7badaa8ffc
Sơn Phạm Văn <vanson9x.it@gmail.com>
2022-01-09 20:31:25 +0700

    chore: initial commit

---
+++ android/src/main/java/com/reactnativebiometricsstatus/BiometricsStatusPackage.java
@@ -0,0 +1,28 @@
+package com.reactnativebiometricsstatus;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BiometricsStatusPackage implements ReactPackage {
+    @NonNull
+    @Override
+    public List<NativeModule> createNativeModules(@NonNull ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+        modules.add(new BiometricsStatusModule(reactContext));
+        return modules;
+    }
+
+    @NonNull
+    @Override
+    public List<ViewManager> createViewManagers(@NonNull ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  BiometricsStatusExample
+//
+
+import Foundation
